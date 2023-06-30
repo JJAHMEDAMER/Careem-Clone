7255a1877b7a037666308202f9db704b4018fb99
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-01 00:49:39 +0300

    clean HeroVideo file

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,14 +1,8 @@
-import { useEffect, useRef } from "react"
-
 export const HeroVideo = () => {
-    const videoRef = useRef(null)
-
-    useEffect(() => {
-        videoRef
-    }, [])
     return (
         <section>
-            <video muted="muted" autoplay="autoplay" playsinline="playsinline" loop="loop"
+            <video
+                muted="muted" autoplay="autoplay" playsinline="playsinline" loop="loop"
                 className="rounded-3xl aspect-4/3 object-cover"
             >
                 <source src="careem_video.mp4" type="video/mp4" />
