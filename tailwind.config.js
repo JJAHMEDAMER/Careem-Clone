028871fff7551edab9dda1a8f0b7e927b1f37782
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-02 00:53:45 +0300

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       aspectRatio: {
-        '4/3': '4 / 3',
+        "4/3": "4 / 3",
+      },
+      colors: {
+        blue: "#3837e4",
+        darkBlue: "#001942",
+        lightGreen: "#d2ffd5",
+        green: "#00493e",
+        purple: "#7f5ffa",
+        paleGreen: "#d6ffea",
       },
     },
   },
   plugins: [],
-}
-
+};
