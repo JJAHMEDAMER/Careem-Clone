f25d5ac3f7c6566db6b78e94864f78c61b2ae178
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-02 05:59:50 +0300

    make info section fit medium screens

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -4,7 +4,7 @@ import { Button } from './subComponent/Button'
 export const InfoSection = () => {
     return (
         <section className='flex flex-col gap-16 text-gray-800'>
-            <div className='flex flex-col gap-4 px-4'>
+            <div className='flex flex-col gap-4 px-4 md:px-36'>
                 <img
                     src="https://upload-cdn.careem.com/About_us_updt_48b263b2f9.png"
                     alt="Office Image"
@@ -15,7 +15,7 @@ export const InfoSection = () => {
                 <Button />
             </div>
             <div className='bg-shape'>
-                <div className='flex flex-col gap-4 px-4'>
+                <div className='flex flex-col gap-4 px-4 md:px-36'>
                     <img
                         src='https://upload-cdn.careem.com/Captain_homepage_582e60a230.png '
                         alt="Office Image"
@@ -26,7 +26,7 @@ export const InfoSection = () => {
                     <Button buttonColor='bg-white'/>
                 </div>
             </div>
-            <div className='flex flex-col gap-4 px-4'>
+            <div className='flex flex-col gap-4 px-4 md:px-36'>
                 <img
                     src='https://upload-cdn.careem.com/Engineering_at_careem_jpg_362a4b3399.png'
                     alt="Office Image"
@@ -36,7 +36,7 @@ export const InfoSection = () => {
                 <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, eos quasi. Officiis ex quod iusto aspernatur accusamus illum doloremque, consequatur dolorem, numquam ipsa quos reprehenderit atque iure? Exercitationem, deleniti quam!</p>
                 <Button />
             </div>
-            <div className='flex flex-col gap-4 px-4'>
+            <div className='flex flex-col gap-4 px-4 md:px-36'>
                 <img
                     src='https://upload-cdn.careem.com/Our_social_impact_jpg_827fcf5f72.png' alt="Office Image"
                     className='rounded-xl'
