9b56bd0783f641b27137705a3a3e31517433fb1a
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-02 07:41:31 +0300

    make footer fit large screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,57 +10,60 @@ import twitterIcon from "../assets/twitter.png"
 
 export const Footer = () => {
   return (
-    <footer className='mx-auto text-center mb-8'>
-      <div className='mb-4 flex flex-col justify-center items-center gap-2'>
-        <img src={googlePlay} alt="googlePlay" className='w-40' />
-        <img src={directAndroid} alt="directAndroid" className='w-40' />
+    <footer className='mx-auto text-center mb-8 '>
+      <div className='lg:flex lg:gap-28'>
+        <div className='mb-4 flex flex-col justify-center items-center gap-2'>
+          <img src={googlePlay} alt="googlePlay" className='w-40' />
+          <img src={directAndroid} alt="directAndroid" className='w-40' />
+        </div>
+        <div className='mb-4'>
+          <h1 className='text-2xl font-bold text-green'>Services</h1>
+          <ul className='font-semibold text-gray-800'>
+            <li>GO</li>
+            <li>Eat</li>
+            <li>Get</li>
+            <li>Pay</li>
+            <li>Careem Plus</li>
+          </ul>
+        </div>
+        <div className='mb-4'>
+          <h1 className='text-2xl font-bold text-green'>Partners</h1>
+          <ul className='font-semibold text-gray-800'>
+            <li>Lorem</li>
+            <li>ipsum</li>
+            <li>dolor</li>
+            <li>amet</li>
+          </ul>
+        </div>
+        <div className='mb-4'>
+          <h1 className='text-2xl font-bold text-green'>Join Our Team</h1>
+          <ul className='font-semibold text-gray-800'>
+            <li>Lorem</li>
+            <li>ipsum</li>
+            <li>dolor</li>
+            <li>amet</li>
+          </ul>
+        </div>
+        <div className='mb-4'>
+          <h1 className='text-2xl font-bold text-green'>About Us</h1>
+          <ul className='font-semibold text-gray-800'>
+            <li>Lorem</li>
+            <li>ipsum</li>
+            <li>dolor</li>
+            <li>amet</li>
+          </ul>
+        </div>
       </div>
-      <div className='mb-4'>
-        <h1 className='text-2xl font-bold text-green'>Services</h1>
-        <ul className='font-semibold text-gray-800'>
-          <li>GO</li>
-          <li>Eat</li>
-          <li>Get</li>
-          <li>Pay</li>
-          <li>Careem Plus</li>
-        </ul>
-      </div>
-      <div className='mb-4'>
-        <h1 className='text-2xl font-bold text-green'>Partners</h1>
-        <ul className='font-semibold text-gray-800'>
-          <li>Lorem</li>
-          <li>ipsum</li>
-          <li>dolor</li>
-          <li>amet</li>
-        </ul>
-      </div>
-      <div className='mb-4'>
-        <h1 className='text-2xl font-bold text-green'>Join Our Team</h1>
-        <ul className='font-semibold text-gray-800'>
-          <li>Lorem</li>
-          <li>ipsum</li>
-          <li>dolor</li>
-          <li>amet</li>
-        </ul>
-      </div>
-      <div className='mb-4'>
-        <h1 className='text-2xl font-bold text-green'>About Us</h1>
-        <ul className='font-semibold text-gray-800'>
-          <li>Lorem</li>
-          <li>ipsum</li>
-          <li>dolor</li>
-          <li>amet</li>
-        </ul>
-      </div>
-      <div>
+
+      <div className='flex justify-between items-center flex-row-reverse lg:mt-10'> 
         <div className='flex gap-12 mb-2'>
           <img src={facebookIcon} alt="facebook" />
           <img src={twitterIcon} alt="twitter" />
           <img src={linkedinIcon} alt="linkedin" />
           <img src={instagramIcon} alt="instagram" />
         </div>
-        <div className='mb-14'>
-          <ul className='flex gap-16 text-xs font-semibold'>
+        <div className='mb-14 lg:mb-0'>
+          <ul className='flex items-center justify-center gap-16 text-xs font-semibold'>
             <li>Terms of Service</li>
             <li>Privacy Policy</li>
           </ul>
