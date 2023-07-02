a498180e62e8c9c473c87d25003f4ee342495fb5
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-02 05:59:29 +0300

    make heroVideo fit medium screens

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -5,7 +5,7 @@ export const HeroVideo = () => {
         <section className="mt-20">
             <video
                 muted autoPlay playsInline loop
-                className="rounded-3xl aspect-4/3 object-cover"
+                className="rounded-3xl w-full object-cover h-72"
             >
                 <source src={careemVideo} type="video/mp4" />
             </video>
