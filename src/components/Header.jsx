5531ef0de3e654fb1a9f967745593cecfc35c49e
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-02 05:39:17 +0300

    add shadow to header on scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+
 
 export const Header = () => {
+    const [scrollTop, setScrollTop] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = event => {
+            setScrollTop(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className='flex px-4 h-20 justify-between items-center fixed z-10 bg-white w-full'>
+        <header className={`${scrollTop ? "shadow-md" : ""} flex px-4 h-20 justify-between items-center fixed z-10 bg-white w-full`}>
             <img className='h-4' src="https://upload-cdn.careem.com/careem_logo_new_36c7f7d5bd.png" alt="Careem Logo" />
             <div className='flex p-4 items-center gap-2'>
                 <button className='px-4 py-2 bg-limeGreen text-gray-800 rounded-lg'>Download the app</button>
