e52014407d581913c2ea4347a532a21daf8ffd8f
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-02 08:06:09 +0300

    make header fit-large screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,8 +18,16 @@ export const Header = () => {
     }, []);
 
     return (
-        <header className={`${scrollTop ? "shadow-md" : ""} flex px-4 h-20 justify-between items-center fixed z-10 bg-white w-full`}>
-            <img className='h-4' src="https://upload-cdn.careem.com/careem_logo_new_36c7f7d5bd.png" alt="Careem Logo" />
+        <header className={`${scrollTop ? "shadow-md" : ""} flex px-4 lg:px-36 h-20 justify-between items-center fixed z-10 bg-white w-full`}>
+            <div className='flex items-center gap-12'>
+                <img className='h-4 md:h-6' src="https://upload-cdn.careem.com/careem_logo_new_36c7f7d5bd.png" alt="Careem Logo" />
+                <ul className='lg:flex gap-4 font-semibold hidden'>
+                    <li>Services</li>
+                    <li>Partners</li>
+                    <li>Careem Plus</li>
+                    <li>About us</li>
+                </ul>
+            </div>
             <div className='flex p-4 items-center gap-2'>
                 <Button text="Download the app" />
                 <div className='flex flex-col gap-1'>
